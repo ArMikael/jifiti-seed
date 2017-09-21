34be10792642a6059858c4d371a93b597f3bb511
Michael Treser <armikael2@gmail.com>
2017-09-21 09:29:33 +0300

    Store and Products factory in separate service.

--- app/store/products.factory.js
+++ app/store/products.factory.js
@@ -6,26 +6,55 @@
 
     //productsFactory.$inject = ['$http'];
 
-    function productsFactory($http) {
+    function productsFactory($http, $rootScope) {
         console.log('FACTORY');
 
         return {
             getProducts: getProducts
         };
 
-        function getProducts() {
-            return $http.get('./data/products.json')
+        function getProducts () {
+            $http.get('./data/products.json')
                 .then(getProductsComplete)
                 .catch(getProductsFailed);
 
             function getProductsComplete(response) {
-                console.log('data received', response.data);
-                return response.data;
+                var stores = response.data.Stores;
+
+                $rootScope.storesList = stores.map(function(store) {
+                    return new StoreFactory(store);
+                });
+
+                function StoreFactory(store){
+                    return {
+                        name: store.StoreName,
+                        logo: store.StoreLogo,
+                        products: createProductsList(store.Products)
+                    }
+                }
+
+                function createProductsList(products) {
+                    return products.map(function (product) {
+                        return new Product(product);
+                    });
+                }
+
+                function Product (product) {
+                    return {
+                        id: product.ProductId,
+                        title: product.ProductTitle,
+                        description: product.Description,
+                        image: product.ProductImage,
+                        price: product.Price,
+                        priceLabel: product.PriceLabel,
+                        tags: product.ProductTags
+                    }
+                }
+
+                console.log('finally', $rootScope.storesList);
             }
 
             function getProductsFailed(error) {
-                console.log('data not received');
-
                 logger.error('XHR Failed for getProducts.' + error.data);
             }
         }
--- app/store/store.controller.js
+++ app/store/store.controller.js
@@ -11,59 +11,10 @@
             });
         }])
 
-        .controller('StoreCtrl', ['$scope', 'productsFactory', '$http', function($scope, productsFactory, $http) {
+        .controller('StoreCtrl', ['$scope', 'productsFactory',
+            function($scope, productsFactory) {
 
-            $scope.storesList = [];
-            $scope.productsList = [];
-
-
-            getProducts();
-
-            function getProducts () {
-                $http.get('./data/products.json')
-                    .then(getProductsComplete)
-                    .catch(getProductsFailed);
-
-                function getProductsComplete(response) {
-                    var stores = response.data.Stores;
-
-                    $scope.storesList = stores.map(function(store) {
-                        return new StoreFactory(store);
-                    });
-
-                    function StoreFactory(store){
-                        return {
-                            name: store.StoreName,
-                            logo: store.StoreLogo,
-                            products: createProductsList(store.Products)
-                        }
-                    }
-
-                    function createProductsList(products) {
-                        return products.map(function (product) {
-                            return new Product(product);
-                        });
-                    }
-
-                    function Product (product) {
-                        return {
-                            id: product.ProductId,
-                            title: product.ProductTitle,
-                            description: product.Description,
-                            image: product.ProductImage,
-                            price: product.Price,
-                            priceLabel: product.PriceLabel,
-                            tags: product.ProductTags
-                        }
-                    }
-
-                    console.log('finally', $scope.productsList);
-                }
-
-                function getProductsFailed(error) {
-                    logger.error('XHR Failed for getProducts.' + error.data);
-                }
-            }
+            productsFactory.getProducts();
 
         }]);
 })( );
