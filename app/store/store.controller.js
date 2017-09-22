3e32a68079a0baa887992420584b18f0c4c95b4e
Michael Treser <armikael2@gmail.com>
2017-09-22 22:14:55 +0300

    Added default values and styles to all filters.

--- app/store/store.controller.js
+++ app/store/store.controller.js
@@ -9,16 +9,37 @@
             });
         }])
 
-        .controller('StoreCtrl', ['$scope', 'productsFactory',
-            function($scope, productsFactory) {
-            $scope.priceFilterModel = '9999';
-            $scope.genderFilterModel = '3';
-
-            $scope.priceFilterMod = '0';
-            $scope.genderFilterMod = '0';
+        .controller('StoreCtrl', ['$scope', 'productsFactory', function($scope, productsFactory) {
+            $scope.priceFilterModel = '0';
+            $scope.genderFilterModel = '0';
 
             // Brings all store data from the server (JSON)
             productsFactory.getStoreData();
+
+            $scope.priceFiltersList = productsFactory.getPriceFilters();
+            $scope.genderFiltersList = productsFactory.getGenderFilters();
+
+            $scope.priceRangeFilter = function (product) {
+                if ($scope.priceFilterModel != 0) {
+                    var borders = $scope.priceFilterModel.split(",");
+                    var minPrice = borders[0],
+                        maxPrice = borders[1];
+
+                    return product.price > minPrice && product.price < maxPrice;
+                }
+
+                return product;
+            };
+
+            $scope.genderTypeFilter = function (product) {
+                if ($scope.genderFilterModel != 0) {
+                    return product.tags.indexOf(parseInt($scope.genderFilterModel)) > -1;
+                }
+
+                return product;
+            };
+
+            // $scope.priceFilterModel = '9999';
 
             // $scope.toggleDropDown = function (event) {
             //     console.log(event);
@@ -26,39 +47,8 @@
 
             // $scope.showPriceFilter = false;
 
-            $scope.priceFiltersList = productsFactory.getPriceFilters();
-            $scope.genderFiltersList = productsFactory.getGenderFilters();
-
-            $scope.priceRangeFilter = function (product) {
-                if ($scope.priceFilterMod != 0) {
-                    var borders = $scope.priceFilterMod.split(",");
-                    var minPrice = borders[0],
-                        maxPrice = borders[1];
-
-                    return product.price > minPrice && product.price < maxPrice;
-                }
-
-                console.log('0 return price');
-                return product;
-            };
-
-            $scope.genderTypeFilter = function (product) {
-                if ($scope.genderFilterMod != 0) {
-                    return product.tags.indexOf(parseInt($scope.genderFilterMod)) > -1;
-                }
-
-                console.log('0 return gender');
-                return product;
-            };
-
-            $scope.priceFilter = function (product) {
-                return product.price < $scope.priceFilterModel;
-            };
-
-            $scope.genderFilter = function (product) {
-                return product.tags.indexOf(parseInt($scope.genderFilterModel)) > -1;
-            };
-
-
+            // $scope.priceFilter = function (product) {
+            //     return product.price < $scope.priceFilterModel;
+            // };
         }]);
 })( );
