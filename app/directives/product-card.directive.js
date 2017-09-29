53cd1ad07259753690ad454b251dbf7af43a91fd
Michael Treser <armikael2@gmail.com>
2017-09-29 22:53:00 +0300

    Updating directive example.

--- app/directives/product-card.directive.js
+++ app/directives/product-card.directive.js
@@ -5,9 +5,15 @@ angular.module('jifitiApp.directives.productCard', [])
            scope: {
              product: '='
            },
+           templateUrl: 'templates/productCard.html',
            // transclude: false, - Will show/hide elements that added in HTML inside the directive
            // replace: true, - will show/hide in the DOM original directive tag or replace it with elements inside the template
-           templateUrl: 'templates/productCard.html',
+           // Use link when we want to watch for DOM events. When you want to add behavior to it when users interacts with directive.
+           link: function (scope, element, attrs) {
+                element.click(function () {
+                    console.log('Click');
+                });
+           },
            controller: function($scope) {
                console.log('contactCard is running');
                //console.log('Directive data: ', $scope.product)
